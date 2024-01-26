e3329a77284f62e50c368e1c9893985941be6643
HenriBueno <henriquetribueno@gmail.com>
2024-01-25 21:53:23 -0300

    PR1 Pull request

--- script.js
+++ script.js
@@ -1,32 +1,7 @@
-// 1. Deve ser desenvolvido um programa que realize um CRUD de carros.
-// Os inputs devem ser feitos através do PROMPT do HTML. O
-// funcionamento deve ser o seguinte:
-// Ao abrir a página, o sistema deve apresentar a seguinte tela:
-
-// Bem-vindo ao sistema de CRUD de veículos:
-
-// No momento, o sistema tem X carros cadastrados
-
-// Escolha uma das opções para interagir com o sistema:
-
-//
-
-// Regras:
-// - Os dados de um veículo são: identificador, modelo, marca,
-// // ano, cor, preco
-// - A opção de filtro deve ser por marca e ordenada pelo preco
-// - A lista de veículos deve estar ordenada pelo preco.
-// - Só deve ser possível atualizar a cor e o preço do veículo.
 
 let menu = 1;
 let veiculos = [];
-// 1 - Cadastrar veículo
-// -> Ao entrar nesta opção o sistema deve pedir os seguintes
-// dados: modelo, marca, ano, cor e preco.
-// -> O veículo deve ser adicionado na lista de veículos que
-// armazena todos os veículos cadastrados
-// -> Todo veículo deve ter um identificador único. Este
-// identificador deve ser gerado de forma automática
+
 function case1() {
   let veiculo = {
     id: veiculos.length + 1,
@@ -38,13 +13,7 @@ function case1() {
   };
   veiculos.push(veiculo);
 }
-// 2 - Listar todos os veículos
-// -> Ao entrar nesta opção o sistema deve listar os veículos
-// com o seguinte layout:
-// ID: 1 | Modelo: Civic| Marca: Honda | Ano: 2014/2015 | Cor: Azul |
-// Preço: R$40.000
-// ID: 1 | Modelo: Civic| Marca: Honda | Ano: 2014/2015 | Cor: Azul |
-// Preço: R$40.000
+
 function case2() {
   veiculos.map((veiculo) => {
     veiculo.id,
@@ -74,13 +43,7 @@ function case2() {
     );
   });
 }
-// 3 - Filtrar veículos por marca
-// -> Ao entrar nesta opção o sistema deve pedir para o
-// usuário digitar a marca que quer filtrar
-// -> Deve ser listado os veículos que forem da mesma marca
-// -> A lista deve ter o seguinte layout:
-// ID: 1 | Modelo: Civic| Cor: Azul | Preço: R$40.000
-// ID: 1 | Modelo: Civic| Cor: Azul | Preço: R$40.000
+
 function case3() {
   let veiculosFiltrados = prompt(
     "Digite a Marca do Veiculo que voce deseja filtrar"
@@ -98,15 +61,7 @@ function case3() {
       console.log(veiculo.id, veiculo.modelo, veiculo.cor, veiculo.preco);
     });
 }
-// 4 - Atualizar veículo
-// -> Ao entrar nesta opção o sistema deve pedir para o
-// usuário digitar o IDENTIFICADOR do veículo
-// -> O Sistema deve verificar se o veículo existe ou não e
-// mostrar a seguinte mensagem caso o veículo não exista:
-// "Veículo, não encontrado. O usuário deve voltar para o menu
-// inicial depois"
-// -> Se o veículo existir, o sistema deve permitir que o usuário
-// atualize somente a cor e o preço.
+
 function case4() {
   let veiculoAtualizado = Number(
     prompt("Digite o  ID do Veiculo que voce deseja atualizar ")
@@ -127,17 +82,9 @@ function case4() {
     );
     return;
   }
-  // let VeiculoCopiado ={...veiculos}
+  
 }
-// 5 - Remover veículo
 
-// -> Ao entrar nesta opção o sistema deve pedir para o
-// usuário digitar o IDENTIFICADOR do veículo
-// -> O Sistema deve verificar se o veículo existe ou não e
-// mostrar a seguinte mensagem caso o veículo não exista:
-// "Veículo, não encontrado. O usuário deve voltar para o menu
-// inicial depois"
-// -> Se o veículo existir, o sistema deve remover o veículo
 function case5() {
   let veiculoIdentificador = Number(
     prompt("Digite o  ID do Veiculo que voce deseja atualizar ")
@@ -157,7 +104,7 @@ function case5() {
   return;
 }
 
-// 6 - Sair do sistema
+
 function lacoDeRepeticao() {
   while (true) {
     const opcao = Number(
